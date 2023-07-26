685c58636d2849f6163c54cfaf4ff28e8e525c03
Elias <75465281+eliskol@users.noreply.github.com>
2023-07-26 02:04:07 +0000

    initialize class

--- hopfield.py
+++ hopfield.py
@@ -1,3 +1,11 @@
+import numpy as np
+
+
 class Hopfield:
-    def __init__(self, n) -> None:
+    def __init__(self, n, weights = ) -> None:
+        # n^2 = number of nodes in the network 
+        self.weights = np.random.choice([-1, 1], size=(n,n))
+        self.values = np.random.choice([-1, 1], size=n)
+
+    def do_instantaneous_update(self):
         pass
