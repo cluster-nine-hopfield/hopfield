18e394afbc179217fa2dbca6a5af2bd1d5f57bc1
jz1010 <24jzweiback@athenian.org>
2023-08-01 15:02:14 -0700

    sync vs async

--- imageclass.py
+++ imageclass.py
@@ -1,5 +1,5 @@
 from hopfield import *
 
-arr = image_nodes("one.png")
-hop = Hopfield(n=784, values=arr)
-print(hop.values)
+#arr = image_nodes("one.png")
+#hop = Hopfield(n=784, values=arr)
+#print(hop.values)
